22b9bd69d3741953de3a71107904483716449ebd
Charulata Mhaiskey <charu.mhaiskey96@gmail.com>
2023-01-16 14:41:30 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(2,5);
+        p.product(1,3,4);
+        p.product(5,7);
+    }
+}
+class Product{
+    public int product(int x, int y) {
+        int prdct=x*y;
+        return prdct;
+    }
+    public int product(int x, int y, int z) {
+        int prdct=x*y*z;
+        return prdct;
+    }
+    public double product(double x, double y) {
+        double product=x*y;
+        return product;
+    }
+
 
 }
