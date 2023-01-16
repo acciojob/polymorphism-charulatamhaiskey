deef8e50face5def18ec7ca462fa0edc0c730b86
Charulata Mhaiskey <charu.mhaiskey96@gmail.com>
2023-01-16 14:50:33 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,25 +2,28 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        Product p=new Product();
-        p.product(2,5);
-        p.product(1,3,4);
-        p.product(5,7);
-    }
-}
-class Product{
-    public int product(int x, int y) {
-        int prdct=x*y;
-        return prdct;
-    }
-    public int product(int x, int y, int z) {
-        int prdct=x*y*z;
-        return prdct;
-    }
-    public double product(double x, double y) {
-        double product=x*y;
-        return product;
+        Product p = new Product();
+
+        int firstansValue = p.product(2, 3);
+        int secondansValue = p.product(1, 3, 4);
+        double thirdansValue = p.product(5, 7);
     }
 
+    static class Product {
+        public int product(int x, int y) {
+            int prdct = x * y;
+            return prdct;
+        }
 
+        public int product(int x, int y, int z) {
+            int prdct = x * y * z;
+            return prdct;
+        }
+
+        public double product(double x, double y) {
+            double prdct = x * y;
+            return prdct;
+        }
+
+    }
 }
